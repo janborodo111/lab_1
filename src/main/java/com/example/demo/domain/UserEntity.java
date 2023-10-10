e90cdac14b8d3c265c2451bee5b65eff863bcf75
janborodo111 <s95548@pollub.edu.pl>
2023-10-10 18:27:51 +0200

    New class UserEntity.java added

---
+++ src/main/java/com/example/demo/domain/UserEntity.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain;
+
+import com.example.demo.DemoApplication;
+import org.springframework.boot.SpringApplication;
+
+public class UserEntity {
+
+    public static void main(String[] args) {
+        System.out.println("UserEntity");
+    }
+}
